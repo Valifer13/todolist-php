bf9f77a49d22d1a3f3ebdbee5e239cf170ba3856
Valifer13 <vianviszello@gmail.com>
2025-02-04 21:38:43 +0800

    fix url rewrite engine

--- app/init.php
+++ app/init.php
@@ -1,5 +1,8 @@
 <?php 
 
+require_once '../app/config/constant.php';
+require_once '../app/config/db.php';
+
 spl_autoload_register(function ($class_name) {
     include '../app/core/' . $class_name . '.php';
 });
