0803f3795f8b6b956d9aedd195d5282f5e8810cc
Valifer13 <vianviszello@gmail.com>
2025-02-06 20:53:25 +0800

    start create hero section demo

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,18 @@
-<div class="container" style="height: 200vh;">
-    <h1>testing</h1>
-</div>
+<section class="container hero" id="hero">
+    <div class="content">
+        <div class="text-content">
+            <h1>Be more productive with new experience of workspace</h1>
+            <p>Infinite adaptation workspace.</p>
+            <div class="btn-group">
+                <button type="button" class="btn btn-black">Register</button>
+                <button type="button" class="btn btn-light">Login</button>
+            </div>
+        </div>
+        <div class="img-content">
+            <img src="https://placehold.co/600x400" alt="teamwork">
+        </div>
+    </div>
+    <div class="showcase">
+        <img src="https://placehold.co/600x400" alt="showcase_app">
+    </div>
+</section>
