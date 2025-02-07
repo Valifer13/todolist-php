c1fb8cf7e8acb6c3d93aeaf8b2e618fd0c8406cf
Valifer13 <vianviszello@gmail.com>
2025-02-07 14:48:23 +0800

    change config structure into one file

--- app/config/config_example.php
+++ app/config/config_example.php
@@ -1,5 +1,9 @@
 <?php 
 
+// Constants
+define("BASE_URL", "<YOUR_BASE_URL>");
+
+// DB Connection
 define("DB_HOST", "<YOUR_DB_HOSTNAME>");
 define("DB_USER", "<YOUR_DB_USERNAME>");
 define("DB_PASS", "<YOUR_DB_PASSWORD>");
--- app/init.php
+++ app/init.php
@@ -1,7 +1,6 @@
 <?php 
 
-require_once '../app/config/constant.php';
-require_once '../app/config/db.php';
+require_once '../app/config/config.php';
 
 spl_autoload_register(function ($class_name) {
     include '../app/core/' . $class_name . '.php';
