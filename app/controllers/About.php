948feb9401b591881ae9e9dd7591de9cf1d00897
Valifer13 <vianviszello@gmail.com>
2025-02-08 17:43:52 +0800

    success get user data from database

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,10 +3,11 @@
 class About extends Controller {
     public function index() {
         $data = [
-            "title"=> "About",
+            "title" => "About",
         ];
+        $data['user'] = $this->model('UserModel')->getUserById(1);
         $this->view('templates/header', $data);
-        $this->view('about/index');
+        $this->view('about/index', $data);
         $this->view('templates/footer');
     }
 }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,1 +1,3 @@
-<h1>This is about page.</h1>
+<br><br><br><br><br>
+<h1>Hi I'm <?= $data['user']['username'] ?>!👋</h1>
+<p>This is about me page.</p>
